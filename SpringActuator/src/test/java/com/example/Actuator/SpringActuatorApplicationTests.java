2d124c50cd9fa328cad5b6b35d34c9805f0aeba6
deraganotra <deraganotra@deraganotra-HP-Notebook>
2021-06-22 11:11:32 +0530

    first commit

---
+++ SpringActuator/src/test/java/com/example/Actuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
